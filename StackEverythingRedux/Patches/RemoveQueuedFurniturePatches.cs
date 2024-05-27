b0a741818a1accbe1f20c3c03f23987c7c61809b
Wren <grahamgrasley@gmail.com>
2024-05-27 04:26:29 -0400

    [SER] QueuedFurniture Transpiler

--- StackEverythingRedux/Patches/RemoveQueuedFurniturePatches.cs
+++ StackEverythingRedux/Patches/RemoveQueuedFurniturePatches.cs
@@ -1,51 +1,108 @@
+using HarmonyLib;
 using StardewValley;
-using StardewValley.Locations;
-using StardewValley.Objects;
+using StardewValley.Inventories;
+using System.Reflection.Emit;
 
 namespace StackEverythingRedux.Patches
 {
     internal class RemoveQueuedFurniturePatches
     {
-        public static bool Prefix(DecoratableLocation __instance, Guid guid)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> source, ILGenerator gen)
         {
-            RemoveQueuedFurniture(__instance, guid);
-            return false;
-        }
+            CodeMatcher il = new CodeMatcher(source, gen);
+            LocalBuilder qualifiedId = gen.DeclareLocal(typeof(string));
+            LocalBuilder item = gen.DeclareLocal(typeof(Item));
+            Label skip = gen.DefineLabel();
+            Label @break = gen.DefineLabel();
 
-        private static void RemoveQueuedFurniture(DecoratableLocation instance, Guid guid)
-        {
-            Farmer who = Game1.player;
-            if (!instance.furniture.TryGetValue(guid, out Furniture furnitureItem) || !who.couldInventoryAcceptThisItem(furnitureItem))
-            {
-                return;
-            }
-            furnitureItem.performRemoveAction();
-            instance.furniture.Remove(guid);
-            bool foundInToolbar = false;
-            for (int j = 0; j < 12; j++)
-            {
-                if (who.Items[j] != null && who.Items[j].QualifiedItemId == furnitureItem.QualifiedItemId)
-                {
-                    who.Items[j].Stack++;
-                    who.CurrentToolIndex = j;
-                    foundInToolbar = true;
-                    break;
-                }
-                else if (who.Items[j] == null)
-                {
-                    who.Items[j] = furnitureItem;
-                    who.CurrentToolIndex = j;
-                    foundInToolbar = true;
-                    break;
-                }
-            }
-            if (!foundInToolbar)
-            {
-                Item item = who.addItemToInventory(furnitureItem, 11);
-                _ = who.addItemToInventory(item);
-                who.CurrentToolIndex = 11;
-            }
-            instance.localSound("coin");
+            // FIND: after furniture.TryGetValue();
+            il.MatchStartForward(
+                new CodeMatch(OpCodes.Ldloc_0),
+                new CodeMatch(OpCodes.Ldloc_1)
+            );
+
+            // ADD: qualifiedId = furniture.QualifiedItemId;
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldloc_1),
+                new CodeInstruction(OpCodes.Callvirt, typeof(Item).GetProperty(nameof(Item.QualifiedItemId)).GetMethod),
+                new CodeInstruction(OpCodes.Stloc, qualifiedId)
+            );
+
+            // FIND: inv[i] != null
+            il.MatchStartForward(
+                new CodeMatch(OpCodes.Ldloc_0),
+                new CodeMatch(OpCodes.Callvirt, typeof(Farmer).GetProperty(nameof(Farmer.Items)).GetMethod),
+                new CodeMatch(OpCodes.Ldloc_3),
+                new CodeMatch(OpCodes.Callvirt, typeof(Inventory).GetMethod("get_Item"))
+            );
+
+            // ADD: code chunk
+            // setup jumps
+
+            il.Advance(1);
+            il.Insert(
+                new CodeInstruction(OpCodes.Ldloc_0).WithLabels(skip)
+            );
+
+            // item = who.Items[i];
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Callvirt, typeof(Farmer).GetProperty(nameof(Farmer.Items)).GetMethod),
+                new CodeInstruction(OpCodes.Ldloc_3),
+                new CodeInstruction(OpCodes.Callvirt, typeof(Inventory).GetMethod("get_Item")),
+                new CodeInstruction(OpCodes.Stloc, item)
+            );
+
+            // if item != null
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldloc, item),
+                new CodeInstruction(OpCodes.Brfalse, skip)
+            );
+
+            // && item.QualifiedItemId == qualifiedId
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldloc, item),
+                new CodeInstruction(OpCodes.Callvirt, typeof(Item).GetProperty(nameof(Item.QualifiedItemId)).GetMethod),
+                new CodeInstruction(OpCodes.Ldloc, qualifiedId),
+                new CodeInstruction(OpCodes.Callvirt, typeof(string).GetMethod(nameof(string.Equals), [typeof(string)])),
+                new CodeInstruction(OpCodes.Brfalse, skip)
+            );
+
+            // item[i].Stack++
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldloc, item),
+                new CodeInstruction(OpCodes.Ldloc, item),
+                new CodeInstruction(OpCodes.Callvirt, typeof(Item).GetProperty(nameof(Item.Stack)).GetMethod),
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Add),
+                new CodeInstruction(OpCodes.Callvirt, typeof(Item).GetProperty(nameof(Item.Stack)).SetMethod)
+            );
+
+            // who.CurrentToolIndex = i;
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Ldloc_3),
+                new CodeInstruction(OpCodes.Callvirt, typeof(Farmer).GetProperty(nameof(Farmer.CurrentToolIndex)).SetMethod)
+            );
+
+            // foundInToolbar = true;
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Stloc_2)
+            );
+
+            // break;
+            il.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Br, @break)
+            );
+
+            // FIND: loop end
+            il.MatchStartForward(
+                new CodeMatch(OpCodes.Blt_S)
+            );
+            il.Advance(1);
+            il.AddLabels([@break]); // attach break
+
+            return il.InstructionEnumeration();
         }
     }
 }
