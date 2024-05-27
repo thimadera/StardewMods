8feccc3c09fe732be7033cb185b51202eca161ea
Wren <grahamgrasley@gmail.com>
2024-05-27 02:58:11 -0400

    [SER] TryToPlaceItem Transpiler

--- StackEverythingRedux/Patches/TryToPlaceItemPatches.cs
+++ StackEverythingRedux/Patches/TryToPlaceItemPatches.cs
@@ -1,111 +1,59 @@
+using HarmonyLib;
 using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Objects;
 using StardewValley.TerrainFeatures;
+using System.Reflection.Emit;
 using SObject = StardewValley.Object;
 
 namespace StackEverythingRedux.Patches
 {
     internal class TryToPlaceItemPatches
     {
-        public static bool Prefix(ref bool __result, GameLocation location, Item item, int x, int y)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> source, ILGenerator gen)
         {
-            __result = TryToPlaceItem(location, item, x, y);
-            return false;
+            CodeMatcher il = new CodeMatcher(source, gen);
+
+            // FIND: return true;
+            il.MatchStartForward(
+                new CodeMatch(OpCodes.Ldc_I4_1),
+                new CodeMatch(OpCodes.Ret)
+            );
+
+            // get jump
+            List<Label> labels = il.Instruction.labels;
+            il.Instruction.labels = null;
+
+            // ADD: AdjustFurnitureStack(item);
+            il.Insert(
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Call, typeof(TryToPlaceItemPatches).GetMethod(nameof(AdjustFurnitureStack)))
+            );
+
+            // move jump
+            if (labels != null && labels.Count != 0)
+                il.Instruction.labels = labels;
+
+            return il.InstructionEnumeration();
         }
 
-        private static bool TryToPlaceItem(GameLocation location, Item item, int x, int y)
+        public static void AdjustFurnitureStack(Item item)
         {
-            if (item is null or Tool)
+            if (item is Furniture f && f.Stack > 1)
             {
-                return false;
-            }
-            Vector2 tileLocation = new(x / 64, y / 64);
-            if (Utility.playerCanPlaceItemHere(location, item, x, y, Game1.player))
-            {
-                if (item is Furniture)
+                Furniture copy = Copy(f);
+                if (copy != null)
                 {
-                    Game1.player.ActiveObject = null;
+                    copy.TileLocation = f.TileLocation;
+                    copy.boundingBox.Value = f.boundingBox.Value;
+                    copy.defaultBoundingBox.Value = f.defaultBoundingBox.Value;
+                    copy.Stack = f.Stack - 1;
+                    copy.updateDrawPosition();
+                    Game1.player.ActiveObject = copy;
                 }
 
-                if (((SObject)item).placementAction(location, x, y, Game1.player))
-                {
-                    Game1.player.reduceActiveItemByOne();
-                }
-                else if (item is Furniture furniture)
-                {
-                    Game1.player.ActiveObject = furniture;
-                }
-                else if (item is Wallpaper)
-                {
-                    return false;
-                }
-
-                if (item is Furniture f && f.Stack > 1)
-                {
-                    Furniture copy = Copy(f);
-                    if (copy != null)
-                    {
-                        copy.TileLocation = f.TileLocation;
-                        copy.boundingBox.Value = f.boundingBox.Value;
-                        copy.defaultBoundingBox.Value = f.defaultBoundingBox.Value;
-                        copy.Stack = f.Stack - 1;
-                        copy.updateDrawPosition();
-                        Game1.player.ActiveObject = copy;
-                    }
-
-                    f.Stack = 1;
-                }
-
-                return true;
+                f.Stack = 1;
             }
-            if (Utility.isPlacementForbiddenHere(location) && item != null && item.isPlaceable())
-            {
-                if (Game1.didPlayerJustClickAtAll(ignoreNonMouseHeldInput: true))
-                {
-                    Game1.showRedMessage(Game1.content.LoadString("Strings\\StringsFromCSFiles:Object.cs.13053"));
-                }
-            }
-            else if (item is Furniture furniture && Game1.didPlayerJustLeftClick(ignoreNonMouseHeldInput: true))
-            {
-                switch (furniture.GetAdditionalFurniturePlacementStatus(location, x, y, Game1.player))
-                {
-                    case 1:
-                        Game1.showRedMessage(Game1.content.LoadString("Strings\\StringsFromCSFiles:Furniture.cs.12629"));
-                        break;
-                    case 2:
-                        Game1.showRedMessage(Game1.content.LoadString("Strings\\StringsFromCSFiles:Furniture.cs.12632"));
-                        break;
-                    case 3:
-                        Game1.showRedMessage(Game1.content.LoadString("Strings\\StringsFromCSFiles:Furniture.cs.12633"));
-                        break;
-                    case 4:
-                        Game1.showRedMessage(Game1.content.LoadString("Strings\\StringsFromCSFiles:Furniture.cs.12632"));
-                        break;
-                }
-            }
-            if (item.Category == -19 && location.terrainFeatures.TryGetValue(tileLocation, out TerrainFeature terrainFeature) && terrainFeature is HoeDirt dirt)
-            {
-                switch (dirt.CheckApplyFertilizerRules(item.QualifiedItemId))
-                {
-                    case HoeDirtFertilizerApplyStatus.HasThisFertilizer:
-                        return false;
-                    case HoeDirtFertilizerApplyStatus.HasAnotherFertilizer:
-                        if (Game1.didPlayerJustClickAtAll(ignoreNonMouseHeldInput: true))
-                        {
-                            Game1.showRedMessage(Game1.content.LoadString("Strings\\StringsFromCSFiles:HoeDirt.cs.13916-2"));
-                        }
-                        return false;
-                    case HoeDirtFertilizerApplyStatus.CropAlreadySprouted:
-                        if (Game1.didPlayerJustClickAtAll(ignoreNonMouseHeldInput: true))
-                        {
-                            Game1.showRedMessage(Game1.content.LoadString("Strings\\StringsFromCSFiles:HoeDirt.cs.13916"));
-                        }
-                        return false;
-                }
-            }
-            _ = Utility.playerCanPlaceItemHere(location, item, x, y, Game1.player, show_error: true);
-            return false;
         }
 
         private static Furniture Copy(Furniture obj)
